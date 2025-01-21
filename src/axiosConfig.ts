d4a1a6e2b8efaafb6b95479457898dbbb3c30096
McLareni <mukola.kosobutski05@gmail.com>
2025-01-21 17:41:32 +0100

    vercel back server

--- src/axiosConfig.ts
+++ src/axiosConfig.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const URL = 'http://localhost:3000';
+export const URL = import.meta.env.VITE_URL;
 // export const URL = 'https://6d9e-213-76-69-206.ngrok-free.app';
 
 axios.defaults.baseURL = URL;
